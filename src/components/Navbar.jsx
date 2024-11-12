1031a2a55958b1d6903fd2bd2271f79f579f25db
Your Name <your-email@example.com>
2024-11-12 14:34:45 +0300

    updated navbar database and browserouter

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,17 +4,32 @@ import { Link } from "react-router-dom";
 export default function Navbar(){
     return(
         <>
-         <div to="container-fluid">
-    <Link to="navbar-brand" href="#">Navbar</Link>
-    <div to="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div to="navbar-nav">
-        <Link to="nav-link active" aria-current="page" href="#">Home</Link>
-        <Link to="nav-link" href="#">Features</Link>
-        <Link to="nav-link" href="#">Pricing</Link>
-        <Link to="nav-link disabled" aria-disabled="true">Disabled</Link>
-      </div>
+        <div>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <Link className="navbar-brand" href="#">Navbar</Link>
+      <span className="navbar-toggler-icon"></span>
+  
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" href="#">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" href="#">Individual</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" href="#">Organisation</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link disabled" aria-disabled="true">Disabled</Link>
+        </li>
+      </ul>
     </div>
   </div>
+</nav>
+        </div>
+
 
         </>
        
